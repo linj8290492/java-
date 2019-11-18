feb9e225e37092227cba7bd1271e73c255dda163
linjun <664678659@qq.com>
2019-11-18 20:31:33 +0800

    项目初始化

---
+++ concurrent-special/chapter1-demo/src/main/java/concurrent/special/chapter1demo/Chapter1DemoApplication.java
@@ -0,0 +1,13 @@
+package concurrent.special.chapter1demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter1DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter1DemoApplication.class, args);
+    }
+
+}
---
+++ concurrent-special/chapter1-demo/src/test/java/concurrent/special/chapter1demo/Chapter1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package concurrent.special.chapter1demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter1DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
