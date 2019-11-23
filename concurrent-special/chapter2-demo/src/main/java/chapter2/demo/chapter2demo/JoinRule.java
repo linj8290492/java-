6a121b81d3ce707111a8e1cd541935aa5dc48d26
linjun <664678659@qq.com>
2019-11-23 19:17:23 +0800

    join规则

--- concurrent-special/chapter2-demo/src/main/java/chapter2/demo/chapter2demo/JoinRule.java
+++ concurrent-special/chapter2-demo/src/main/java/chapter2/demo/chapter2demo/JoinRule.java
@@ -10,12 +10,30 @@ public class JoinRule {
     static int x = 0;
 
     public static void main(String[] args) throws InterruptedException {
+//        Thread t1 = new Thread(()->{
+//            x=100;
+//        });
+//        t1.start();
+//        t1.join();//没这段join，x=0;happends-before的体现
+//        System.out.println(x);
+
         Thread t1 = new Thread(()->{
-            x=100;
+            System.out.println("t1");
         });
+        Thread t2 = new Thread(()->{
+            System.out.println("t2");
+        });
+        Thread t3 = new Thread(()->{
+            System.out.println("t3");
+        });
+
         t1.start();
-        t1.join();//没这段join，x=0;happends-before的体现
-        System.out.println(x);
+        t1.join();
+        t2.start();
+        t2.join();
+        t3.start();
+
+
 
     }
 }
