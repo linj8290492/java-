58b38598db841de8762a376217f77aef5d007157
linjun <664678659@qq.com>
2019-12-04 21:45:25 +0800

    Lock锁

---
+++ concurrent-special/chapter6-demo/src/main/java/chapter6/demo/chapter6demo/Chapter6DemoApplication.java
@@ -0,0 +1,13 @@
+package chapter6.demo.chapter6demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter6DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter6DemoApplication.class, args);
+    }
+
+}
---
+++ concurrent-special/chapter6-demo/src/test/java/chapter6/demo/chapter6demo/Chapter6DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package chapter6.demo.chapter6demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter6DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
